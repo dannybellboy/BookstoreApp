c541bf3ff542da894b8c93d28f2857b5ce5813f7
dannybellboy <90338634+dannybellboy@users.noreply.github.com>
2022-02-23 22:44:18 -0700

    mission 8

---
+++ Bookstore/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Bookstore.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+
+        public int BooksPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        //Figure out num pages
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
