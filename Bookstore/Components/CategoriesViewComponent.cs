183e60a0ebbaf846266e3983a3043273efc9c544
dannybellboy <90338634+dannybellboy@users.noreply.github.com>
2022-02-25 11:34:58 -0700

    Mission 8 final update

---
+++ Bookstore/Components/CategoriesViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private IBookstoreRepository repo { get; set; }
+
+        public CategoriesViewComponent (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke ()
+        { 
+            ViewBag.SelectedCategory = RouteData?.Values["Category"];
+
+            var categories = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+
+            return View(categories);
+        }
+    }
+}
